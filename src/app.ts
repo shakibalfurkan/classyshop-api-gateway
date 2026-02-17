5c38bf1c7d0493e375b438705c8f14ffd570bbf9
shakibalfurkan <shakibalfurkan@gmail.com>
2026-02-17 18:41:54 +0600

    requestId middleware added

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,20 @@ import express, { type Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import compression from "compression";
 import { rateLimit } from "express-rate-limit";
 import httpProxy from "express-http-proxy";
 import { setupSecurityMiddleware } from "./middlewares/security.js";
+import { requestIdMiddleware } from "./middlewares/requestId.js";
 
 function createApp(): Application {
   const app: Application = express();
 
   setupSecurityMiddleware(app);
+
+  app.use(compression());
+
+  app.use(requestIdMiddleware);
 
   return app;
 }
